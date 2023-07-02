a6ce05e2b30861276d47d936520638bf019eaf3f
sadeg1377 <sadeghhadipour1@gmail.com>
2023-07-02 17:52:37 +0330

    create project and delete some extra component and add v-expansion-panels

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="HomeView" class="mx-auto mt-8">
+        <h1 class="mb-2">کارمندان</h1>
+        <v-expansion-panels focusable>
+            <v-expansion-panel
+                v-for="(item,i) in 5"
+                :key="i"
+            >
+                <v-expansion-panel-header>Item</v-expansion-panel-header>
+                <v-expansion-panel-content>
+
+                </v-expansion-panel-content>
+            </v-expansion-panel>
+        </v-expansion-panels>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+    name: 'HomeView',
+
+    components: {},
 }
 </script>
+<style scoped>
+#HomeView {
+    width: 60vw;
+}
+</style>
